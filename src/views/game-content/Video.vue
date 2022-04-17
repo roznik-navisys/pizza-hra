0f130b10e947b248667ba14f5d5f0fdf473140c9
roznik-navisys <roznik@navisys.cz>
2022-04-17 11:00:44 +0200

    feat: video comments

--- src/views/game-content/Video.vue
+++ src/views/game-content/Video.vue
@@ -37,21 +37,57 @@
             </span>
         </div>
     </div>
-    <div class="flex flex-row gap-3 pt-4">
-        <img :src="denikLogo" class="w-10 h-10">
-        <div class="flex flex-col items-start text-[0.9375rem]">
-            <span class="font-bold">
-                Nový deník
-            </span>
-            <span class="text-gray-500">
-                214 odběratelů
+    <div class="flex flex-col gap-5 pt-4">
+        <div class="flex flex-row gap-3">
+            <img :src="denikLogo" class="w-10 h-10">
+            <div class="flex flex-col items-start text-[0.9375rem]">
+                <span class="font-bold">
+                    Nový deník
+                </span>
+                <span class="text-gray-500">
+                    214 odběratelů
+                </span>
+            </div>
+            <span class="text-zsi-500 font-bold self-center ml-auto mr-3">
+                ODEBÍRAT
             </span>
         </div>
-        <span class="text-zsi-500 font-bold self-center ml-auto mr-3">
-            ODEBÍRAT
-        </span>
+        <div class="text-lg text-left">
+            <p>
+                Večírek rappera McWesta, kde se odehrála populární kauza ananasové pizzy.
+            </p>
+            <p>
+                Více na <span class="text-zsi-500 underline"> www.novydenik.cz </span>
+            </p>
+        </div>
     </div>
-</div>
+    <div class="pt-4 text-left text-[0.9375rem] font-bold -mb-4">
+        Komentáře <span class="text-gray-500 pl-3"> 816 </span>
+        <div class="flex flex-row gap-4 pt-8 pb-4">
+            <img :src="kaja" class="w-10 h-10">
+            <div class="flex flex-col gap-1 items-start text-[0.9375rem]">
+                <span class="font-bold">
+                    Kája
+                </span>
+                <p class="text-lg font-normal text-left">
+                    Očividně se párty odehrála dřív než samotný incident s pizzou
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-row gap-4 pt-8">
+        <img :src="gabinka" class="w-10 h-10">
+            <div class="flex flex-col gap-1 items-start text-[0.9375rem]">
+                <span class="font-bold">
+                    Gábinka
+                </span>
+                <p class="text-lg text-left">
+                    V celém videu nejde nikomu vidět do obličeje
+                </p>
+            </div>
+        </div>
+    <span />
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -62,6 +98,8 @@ import dislikeIcon from '../../assets/dislike-icon.svg';
 import repeatIcon from '../../assets/repeat-icon.svg';
 import downloadIcon from '../../assets/download-icon.svg';
 import denikLogo from '../../assets/denik-logo.png'
+import gabinka from '../../assets/gabinka.png'
+import kaja from '../../assets/kaja.png'
 
 const group = localStorage.getItem('group')
 </script>
