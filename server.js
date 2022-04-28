05ae460172c690436e69b60c57ef6d0516acaf08
roznik-navisys <roznik@navisys.cz>
2022-04-28 21:17:51 +0200

    fix: port -> PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const express = require('express');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(__dirname + '/dist/'));
