7022b112cea1803e08f9755dd44c77e663b35d0e
roznik-navisys <roznik@navisys.cz>
2022-05-02 19:54:28 +0200

    feat: more intro top padding

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex flex-1 flex-col justify-between py-4">
-        <h1 class="text-zsi-500 text-lg font-moreno font-semibold mt-3" :to="{ name: 'Game' }">
+        <h1 class="text-zsi-500 text-lg font-moreno font-semibold mt-4" :to="{ name: 'Game' }">
             Skupina {{ group }}
         </h1>
         <image-large :image="groupIntro" />
-        <div class="text-[1.0625rem] flex flex-col gap-8 px-4">
+        <div class="text-[1.0625rem] flex flex-col gap-8 px-4 pt-4">
             <p>
                 Nyní jste rozděleni do <strong>dvou skupin</strong>.
             </p>
@@ -15,7 +15,7 @@
                 Zaznamenávejte si všechny podstatné informace a data. Budou se vám hodit, až budete své závěry prezentovat ostatním.
             </p>
         </div>
-        <div class="mt-3">
+        <div class="mt-4">
             <button-primary @click="router.push({ name: 'ArticleIntro' })">
                 Začít
             </button-primary>
